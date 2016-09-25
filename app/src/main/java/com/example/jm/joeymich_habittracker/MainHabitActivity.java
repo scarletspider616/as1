18deab970b0483684e61037e752e79b6001d64bf
scarletspider616 <j.fallone@ualberta.ca>
2016-09-25 14:24:30 -0600

    Cleaned up main activity. Changing my plan of attack for the GUI.
    While previously a spinner was used to change date, I've decided to create
    another button called "change day" to lead into an activity where the day is
    chosen (out of 7 day week)
    
    TODO:
    change day activity
    clickable habit list (main activity)
    individual habit activity (with list of completions, another activity
    to delete a completion)

--- app/src/main/java/com/example/jm/joeymich_habittracker/MainHabitActivity.java
+++ app/src/main/java/com/example/jm/joeymich_habittracker/MainHabitActivity.java
@@ -6,8 +6,10 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 import com.google.android.gms.appindexing.Action;
@@ -16,6 +18,7 @@ import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import static android.app.PendingIntent.getActivity;
 
@@ -25,6 +28,7 @@ public class MainHabitActivity extends AppCompatActivity {
     private String[] daysList;
     private Button newHabit;
     private ArrayList<Habit> habitList;
+    private ArrayAdapter<Habit> habitAdapter;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -38,10 +42,10 @@ public class MainHabitActivity extends AppCompatActivity {
 
         this.daysList = new String[]{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
                 "Friday", "Saturday"};
-        Spinner dayChoice = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, daysList);
-        dayChoice.setAdapter(adapter);
+//        Spinner dayChoice = (Spinner) findViewById(R.id.spinner);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_spinner_item, daysList);
+//        dayChoice.setAdapter(adapter);
 
         // create new habit button.
         newHabit = (Button) findViewById(R.id.newHabit);
@@ -55,48 +59,5 @@ public class MainHabitActivity extends AppCompatActivity {
 
             }
         });
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-    }
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("MainHabit Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
-
-    public void addHabit(Habit addNewHabit) {
-        habitList.add(addNewHabit);
     }
 }
