319ef28f852c6f6c786cf3ad35dd0b22b0e5586d
scarletspider616 <j.fallone@ualberta.ca>
2016-09-26 16:03:52 -0600

    Created intents to transfer day information to main activity TODO: main activity will take taht information and call a new saving object to deal with keeping track of all habits as well as saving to a local file.

--- app/src/main/java/com/example/jm/joeymich_habittracker/ChooseDay.java
+++ app/src/main/java/com/example/jm/joeymich_habittracker/ChooseDay.java
@@ -3,22 +3,33 @@ package com.example.jm.joeymich_habittracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+http://theopentutorials.com/tutorials/android/listview/android-multiple-selection-listview/
+ */
 
 public class ChooseDay extends AppCompatActivity {
 
     private ListView daysList;
     private String[] dayStrings;
     private ArrayAdapter<String> dayAdapter;
+    private String description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_day);
         Intent saveDescription = getIntent();
+        description = saveDescription.getStringExtra("description");
+
         TextView message = (TextView) findViewById(R.id.chooseDayMessage);
         message.setText(R.string.chooseDays);
 
@@ -36,6 +47,16 @@ public class ChooseDay extends AppCompatActivity {
 
     }
 
+    public void onClick(View v) {
+        // http://theopentutorials.com/tutorials/android/listview/android-multiple-selection-listview/
+        SparseBooleanArray checked = daysList.getCheckedItemPositions();
+        ArrayList<Integer> daysSelected = new ArrayList<Integer>();
+        for (int i = 0; i < checked.size(); i++) {
+            daysSelected.add(i);
+        }
+        this.headBackToMainScreen(daysSelected);
+    }
+
     private void populateStrings(String[] inStrings) {
         inStrings[0] = "Sunday";
         inStrings[1] = "Monday";
@@ -44,5 +65,11 @@ public class ChooseDay extends AppCompatActivity {
         inStrings[4] = "Thursday";
         inStrings[5] = "Friday";
         inStrings[6] = "Saturday";
+    }
+
+    private void headBackToMainScreen(ArrayList<Integer> daysSelected) {
+        Intent nIntent = new Intent(this, MainHabitActivity.class);
+        nIntent.putIntegerArrayListExtra("days", daysSelected);
+        nIntent.putExtra("description", description);
     }
 }
