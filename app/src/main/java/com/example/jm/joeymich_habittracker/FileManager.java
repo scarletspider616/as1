665a37945b321a9a70f4b52b0e3f86e5e591bf46
scarletspider616 <j.fallone@ualberta.ca>
2016-09-28 20:18:28 -0600

    Working on some bugs.

--- app/src/main/java/com/example/jm/joeymich_habittracker/FileManager.java
+++ app/src/main/java/com/example/jm/joeymich_habittracker/FileManager.java
@@ -6,6 +6,7 @@ import com.google.gson.Gson;
 
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -42,10 +43,7 @@ public class FileManager {
 
         try {
             InputStream inRead = this.context.openFileInput(filename);
-
-            String data = this.dumpData(inRead);
-            this.populateHabits(data);
-
+            this.dumpData(inRead);
             inRead.close();
         } catch (Exception e) {
             e.printStackTrace();
@@ -53,21 +51,26 @@ public class FileManager {
 
     }
 
-    private String dumpData(InputStream inData) {
-        String dump = "";
-        try {
-            while (inData.available() > 0) {
-                try {
-                    dump = dump + inData.read();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    break;
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void dumpData(InputStream inData) {
+        /**
+         * This work, "dumpData," is a derivative of
+         * "Read String line by line in Java" by "notnoop," and edited by "gregko, "
+         * both stack overflow users, used under CC-BY-SA by Joey-Michael Fallone.
+         * (Available here:
+         * http://stackoverflow.com/questions/1096621/read-string-line-by-line-in-java)
+         *
+         */
+        ArrayList<String> strings = new ArrayList<String>();
+        Scanner scanner = new Scanner(new InputStreamReader(inData));
+        while (scanner.hasNextLine()) {
+            strings.add(scanner.nextLine());
         }
-        return dump;
+        scanner.close();
+
+        // now update habitList
+        for (String string: strings) {
+            this.habitList.add(converter.fromJson(string, Habit.class));
+        }
     }
 
     private void updateFile() {
@@ -78,13 +81,14 @@ public class FileManager {
              * https://developer.android.com/training/basics/data-storage/files.html)
              *
          */
+
+        this.context.deleteFile(filename);
         OutputStream outWrite = null;
         try {
             outWrite = this.context.openFileOutput(filename, Context.MODE_PRIVATE);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        this.context.deleteFile(filename);
         for (Habit habit: this.habitList) {
             String tempConverted = converter.toJson(habit);
             try {
@@ -105,32 +109,23 @@ public class FileManager {
         this.updateFile();
     }
 
-    private void populateHabits(String data) {
-        /**
-         * This work, "populateHabits," is a derivative of
-         * "Read String line by line in Java" by "notnoop," and edited by "gregko, "
-         * both stack overflow users, used under CC-BY-SA by Joey-Michael Fallone.
-         * (Available here:
-         * http://stackoverflow.com/questions/1096621/read-string-line-by-line-in-java)
-         *
-         */
-        // wish this was python...
-        ArrayList<String> strings = new ArrayList<String>();
-        Scanner scanner = new Scanner(data);
-        while (scanner.hasNextLine()) {
-            strings.add(scanner.nextLine());
-        }
-        scanner.close();
-
-        // now update habitList
-        for (String string:strings) {
-            this.habitList.add(converter.fromJson(string, Habit.class));
-        }
-    }
-
     public ArrayList<Habit> getHabitList() {
         return this.habitList;
+//        return this.getSaveString();
     }
+
+//    public ArrayList<Habit> getSaveString() {
+//        String result = "";
+//        try {
+//            InputStream inRead = this.context.openFileInput(filename);
+//            result = dumpData(inRead);
+//        } catch (Exception e) {
+//            // pass
+//        }
+//        this.habitList = new ArrayList<Habit>();
+//        this.habitList.add(new Habit(result, 0));
+//        return this.habitList;
+//    }
 
 }
 
